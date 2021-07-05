b1028b624ae35ad3eb93e3dee44742c23ea46f9c
danielr <danielr@TI-DANIELR-LAP>
2021-07-05 17:08:02 -0500

    Agregar archivos de proyecto.

---
+++ udemyAppAdminEmpleados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(udemyAppAdminEmpleados.Startup))]
+namespace udemyAppAdminEmpleados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
